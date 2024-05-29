37ffb82f6eb73026cbf75aa6101f0af4184fd7e0
Your Name <you@example.com>
2024-05-29 19:22:38 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,59 @@
+// const heading = document.createElement("h1")
+// const text = document.createTextNode("SALOM")
+
+// heading.appendChild(text)
+// heading.style.color = "red"
+
+// const main = document.querySelector("body")
+// main.appendChild(heading)
+
+    
+// const heading2 = document.createElement("h2")
+// const text2 = document.createTextNode("hello")
+
+// heading2.appendChild(text2)
+// heading2.style.color = "red"
+
+// main.appendChild(heading2)
+
+// const heading3 = document.createElement("h3")
+// const text3 = document.createTextNode("privet")
+
+// heading3.appendChild(text3)
+// heading3.style.color = "red"
+
+// main.appendChild(heading3)
+
+// const heading4 = document.createElement("h4")
+// const text4 = document.createTextNode("raxmat")
+
+// heading4.appendChild(text4)
+// heading4.style.color = "red"
+
+// main.appendChild(heading4)
+
+
+// const heading5 = document.createElement("h5")
+// const text5 = document.createTextNode("arzimidi")
+
+// heading5.appendChild(text5)
+// heading5.style.color = "red"
+
+// main.appendChild(heading5)
+
+// const heading6 = document.createElement("h6")
+// const text6 = document.createTextNode("???")
+
+// heading6.appendChild(text6)
+// heading6.style.color = "red"
+
+// main.appendChild(heading6)
+
+
+let a = prompt("Isim kiriting")
+
+// let random = Math.floor(Math.random()*100)
+
+let h1 = document.querySelector("h1")
+
+h1.innerHTML = a;
